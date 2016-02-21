eef928f295ac2287dd1dc820922ff71d73763351
BP Sayler <gotgamble@gmail.com>
2016-02-21 18:04:32 +0100

    First commit

---
+++ if_elif_example.py
@@ -0,0 +1,11 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice, kid')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead imortal vampire')
+elif age > 100:
+    print('You are not Alice, grannie.')
+    
